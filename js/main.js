77a8b0b2153b71242796fee3828bf144ae60c6e7
milan978gh <milan978@gmail.com>
2020-06-18 22:34:14 +0200

    main.js done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// animate smooth scrool
+
+$('#view-work').on('click', function() {
+    const images = $('#images').position().top;
+
+    $('html, body').animate(
+        {
+        scrollTop: images
+        },
+        900
+    );
+});
